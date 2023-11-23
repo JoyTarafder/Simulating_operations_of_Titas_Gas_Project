9031f04bdedf01a74627d35b7e1ca12f91e0b85a
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-11-23 11:08:10 +0600

    updates

--- Simulating operations of Titas Gas/src/mainpkg/LoginPageSceneController.java
+++ Simulating operations of Titas Gas/src/mainpkg/LoginPageSceneController.java
@@ -4,9 +4,20 @@
  */
 package mainpkg;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +26,11 @@ import javafx.fxml.Initializable;
  */
 public class LoginPageSceneController implements Initializable {
 
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private PasswordField passwordTextField;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +38,27 @@ public class LoginPageSceneController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void logInButtonOnAction(ActionEvent event) throws IOException {
+        String username = userNameTextField.getText();
+        String password = passwordTextField.getText();
+        
+        if(username.equals("customer") && password.equals("password")){
+            showAlert("Login Sccessfull!", Alert.AlertType.INFORMATION);
+            Parent parent = FXMLLoader.load(getClass().getResource("CustomerDeshboard.fxml"));
+            Scene newScene = new Scene (parent);
+            Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            newStage.setScene(newScene);
+            newStage.show();
+        }
+    }
+    private void showAlert(String login_sccessful, Alert.AlertType alertType){
+        Alert alert = new Alert(alertType);
+        alert.setTitle("Login Result");
+        alert.setHeaderText(null);
+        alert.setContentText(login_sccessful);
+        alert.showAndWait();
+    }
     
 }
