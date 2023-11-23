3d5958377b6f1456d9c8b536a3a85c47cf0e4b53
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-11-23 20:40:51 +0600

    log in code

--- Simulating operations of Titas Gas/src/mainpkg/LoginPageSceneController.java
+++ Simulating operations of Titas Gas/src/mainpkg/LoginPageSceneController.java
@@ -15,6 +15,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -33,15 +34,18 @@ public class LoginPageSceneController implements Initializable {
     private PasswordField passwordTextField;
     @FXML
     private CheckBox showPasswordCheckBox;
+    @FXML
+    private Button showbutton;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
 
+    }    
+//---------------------------------------------Log In code Start ---------------------------------------------------------------------------------------------
+    
     @FXML
     private void logInButtonOnAction(ActionEvent event) throws IOException {
         String username = userNameTextField.getText();
@@ -49,27 +53,59 @@ public class LoginPageSceneController implements Initializable {
         
         if(username.equals("customer") && password.equals("password")){
             showAlert("Login Sccessfull!", Alert.AlertType.INFORMATION);
-            Parent parent = FXMLLoader.load(getClass().getResource("CustomerDeshboard.fxml"));
-            Scene newScene = new Scene (parent);
+            Parent CustomerDeshboard = FXMLLoader.load(getClass().getResource("CustomerDeshboard.fxml"));
+            Scene newScene = new Scene (CustomerDeshboard);
+            Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            newStage.setScene(newScene);
+            newStage.show();
+        }
+        else if(username.equals("financialmanager") && password.equals("password")){
+            showAlert("Login Sccessfull!", Alert.AlertType.INFORMATION);
+            Parent FinancialManagerDeshboard = FXMLLoader.load(getClass().getResource("FinancialManagerDeshboard.fxml"));
+            Scene newScene = new Scene (FinancialManagerDeshboard);
             Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
             newStage.setScene(newScene);
             newStage.show();
         }
     }
+//---------------------------------------------Log In code End ---------------------------------------------------------------------------------------------
+    
+//------------------------------------- Alert Code Start ---------------------------------------------------------
+    
     private void showAlert(String login_sccessful, Alert.AlertType alertType){
         Alert alert = new Alert(alertType);
         alert.setTitle("Login Result");
-        alert.setHeaderText(null);
         alert.setContentText(login_sccessful);
         alert.showAndWait();
     }
-
+//------------------------------------- Alert Code End ---------------------------------------------------------
+    
+    
+//--------------------------------Don't touch this code -------------------------------------------------
     @FXML
     private void showPasswordCheckBoxOnClick(ActionEvent event) {
+//        if (showPasswordCheckBox.isSelected()) {
+//            passwordTextField.setEchoChar((char)0); //password = JPasswordField
+//         } 
+//        else {
+//            passwordTextField.setEchoChar('*');
+//         }
     }
 
     @FXML
     private void forgotPasswordLinkOnClick(ActionEvent event) {
     }
-    
+
+    @FXML
+    private void showButtonOnClick(ActionEvent event) {
+//        if(showbutton.isSelected()){
+//            passwordTextField.setEchoChar((char)0); //By this line of code. We will actually see the actual characters
+//            showbutton.setText("Hide");
+//        }
+//        else{
+//            passwordTextField.setEchoChar('u25cf');
+//            showbutton.setText("Show");
+//        }
+    }
+//-------------------------------------------- x x x x x -----------------------------------------------------------------------------------------    
 }
