70397ea6de72abb8a16c21ad0d0ee247a4f37f50
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-11-28 19:21:37 +0600

    update

---
+++ Simulating operations of Titas Gas/src/mainpkg/FinancialManagerDeshboardController.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asus
+ */
+public class FinancialManagerDeshboardController implements Initializable {
+
+    @FXML
+    private PieChart pieChart;
+    @FXML
+    private Label nameTextField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void analyzingReportButtonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void financialReportButtonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void grossProfitReportButtonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void trackingExpensesReportButtonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void sendInvoiceCustomerButtonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void yearlyBudgetRecordButtonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void salaryDistributionButtonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void collaborationEmployeeMessageButtonOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void signOutButtonOnAction(ActionEvent event) {
+    }
+    
+}
