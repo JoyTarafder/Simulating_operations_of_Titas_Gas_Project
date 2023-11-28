b8977315fe7bc4c76e8dc8fbf326743791ccdd6f
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-11-28 19:35:51 +0600

    Update Financial Manager Dashboard

--- Simulating operations of Titas Gas/src/mainpkg/FinancialManagerDeshboardController.java
+++ Simulating operations of Titas Gas/src/mainpkg/FinancialManagerDeshboardController.java
@@ -1,21 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
+
 package mainpkg;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author Asus
+ * @author Joy Tarafder
  */
 public class FinancialManagerDeshboardController implements Initializable {
 
@@ -33,39 +36,84 @@ public class FinancialManagerDeshboardController implements Initializable {
     }    
 
     @FXML
-    private void analyzingReportButtonOnAction(ActionEvent event) {
+    private void analyzingReportButtonOnAction(ActionEvent event) throws IOException {
+        Parent AnalyzingReport = FXMLLoader.load(getClass().getResource("FinancialManagerCreateAnalyzingReport.fxml"));
+        Scene newScene = new Scene (AnalyzingReport);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
 
     @FXML
-    private void financialReportButtonOnAction(ActionEvent event) {
+    private void financialReportButtonOnAction(ActionEvent event) throws IOException {
+        Parent FinancialReport = FXMLLoader.load(getClass().getResource("FinancialManagerCreateFinancialReport.fxml"));
+        Scene newScene = new Scene (FinancialReport);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
 
     @FXML
-    private void grossProfitReportButtonOnAction(ActionEvent event) {
+    private void grossProfitReportButtonOnAction(ActionEvent event) throws IOException {
+        Parent GrossProfitReport = FXMLLoader.load(getClass().getResource("FinancialManagerCreateGrossProfitReport.fxml"));
+        Scene newScene = new Scene (GrossProfitReport);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
 
     @FXML
-    private void trackingExpensesReportButtonOnAction(ActionEvent event) {
+    private void trackingExpensesReportButtonOnAction(ActionEvent event) throws IOException {
+        Parent TrackingExpensesReport = FXMLLoader.load(getClass().getResource("FinancialManagerCreateTrackingExpensesReport.fxml"));
+        Scene newScene = new Scene (TrackingExpensesReport);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
 
     @FXML
-    private void sendInvoiceCustomerButtonOnAction(ActionEvent event) {
+    private void sendInvoiceCustomerButtonOnAction(ActionEvent event) throws IOException {
+        Parent SendInvoiceCustomer = FXMLLoader.load(getClass().getResource("FinancialMangerSendInvoiceCustomer.fxml"));
+        Scene newScene = new Scene (SendInvoiceCustomer);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
 
     @FXML
-    private void yearlyBudgetRecordButtonOnAction(ActionEvent event) {
+    private void yearlyBudgetRecordButtonOnAction(ActionEvent event) throws IOException {
+        Parent YearlyBudgetRecord = FXMLLoader.load(getClass().getResource("FinancialManagerCreateYearlyBudgetRecord.fxml"));
+        Scene newScene = new Scene (YearlyBudgetRecord);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
 
     @FXML
-    private void salaryDistributionButtonOnAction(ActionEvent event) {
+    private void salaryDistributionButtonOnAction(ActionEvent event) throws IOException {
+        Parent SalaryDistribution = FXMLLoader.load(getClass().getResource("FinancialManagerSalaryDistribution.fxml"));
+        Scene newScene = new Scene (SalaryDistribution);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
 
     @FXML
-    private void collaborationEmployeeMessageButtonOnAction(ActionEvent event) {
+    private void collaborationEmployeeMessageButtonOnAction(ActionEvent event) throws IOException {
+        Parent CollaborationEmployeeMessage = FXMLLoader.load(getClass().getResource("CollaborationEmployeeMessage.fxml"));
+        Scene newScene = new Scene (CollaborationEmployeeMessage);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
 
     @FXML
-    private void signOutButtonOnAction(ActionEvent event) {
+    private void signOutButtonOnAction(ActionEvent event) throws IOException {
+        Parent SignOut = FXMLLoader.load(getClass().getResource("LoginPageScene.fxml"));
+        Scene newScene = new Scene (SignOut);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
     }
     
 }
