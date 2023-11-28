49b641737ce1d183e56060e6d6659774b7c1c6ce
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-11-28 22:23:06 +0600

    payment Scene update

--- Simulating operations of Titas Gas/src/mainpkg/CustomerBillPaymentController.java
+++ Simulating operations of Titas Gas/src/mainpkg/CustomerBillPaymentController.java
@@ -4,13 +4,19 @@
  */
 package mainpkg;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,14 +25,10 @@ import javafx.scene.control.ToggleGroup;
  */
 public class CustomerBillPaymentController implements Initializable {
 
-    @FXML
-    private RadioButton bankRadioButton;
-    @FXML
-    private RadioButton bkashRadioButton;
-    @FXML
-    private RadioButton rocketRadioButton;
-    @FXML
-    private RadioButton nagadRadioButton;
+    @FXML private RadioButton bankRadioButton;
+    @FXML private RadioButton bkashRadioButton;
+    @FXML private RadioButton rocketRadioButton;
+    @FXML private RadioButton nagadRadioButton;
 
 
     ToggleGroup tg;
@@ -41,7 +43,35 @@ public class CustomerBillPaymentController implements Initializable {
     }    
 
     @FXML
-    private void paymentButtonOnAction(ActionEvent event) {
+    private void paymentButtonOnAction(ActionEvent event) throws IOException {
+        if(bkashRadioButton.isSelected()){
+        Parent BkashPayment = FXMLLoader.load(getClass().getResource("BkashScene.fxml"));
+        Scene newScene = new Scene (BkashPayment);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
+        }
+        else if(bankRadioButton.isSelected()){
+        Parent BkashPayment = FXMLLoader.load(getClass().getResource("BankScene.fxml"));
+        Scene newScene = new Scene (BkashPayment);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
+        }
+        else if(rocketRadioButton.isSelected()){
+        Parent BkashPayment = FXMLLoader.load(getClass().getResource("RocketScene.fxml"));
+        Scene newScene = new Scene (BkashPayment);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
+        }
+        else if(nagadRadioButton.isSelected()){
+        Parent BkashPayment = FXMLLoader.load(getClass().getResource("NagadScene.fxml"));
+        Scene newScene = new Scene (BkashPayment);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
+        }
     }
     
 }
