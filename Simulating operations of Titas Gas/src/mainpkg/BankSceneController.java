e20cb328e0d29cb7586ceb7afffd2787c010d488
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2023-11-29 12:16:12 +0600

    update

---
+++ Simulating operations of Titas Gas/src/mainpkg/BankSceneController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asus
+ */
+public class BankSceneController implements Initializable {
+
+    @FXML
+    private TextField amountTextField;
+    @FXML
+    private DatePicker datePicker;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void payButtonOnAction(ActionEvent event) {
+    }
+    
+}
---
+++ Simulating operations of Titas Gas/src/mainpkg/CustomerPaymentHistoryController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mainpkg;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asus
+ */
+public class CustomerPaymentHistoryController implements Initializable {
+
+    @FXML
+    private TableView<?> paymentHistoryTableView;
+    @FXML
+    private TableColumn<?, ?> dateTableView;
+    @FXML
+    private TableColumn<?, ?> amountTableView;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void backButtonOnAction(ActionEvent event) throws IOException {
+        Parent BackBtn = FXMLLoader.load(getClass().getResource("CustomerDeshboard.fxml"));
+        Scene newScene = new Scene (BackBtn);
+        Stage newStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        newStage.setScene(newScene);
+        newStage.show();
+    }
+    
+}
